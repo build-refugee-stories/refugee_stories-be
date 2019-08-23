5ad477a967844bd6532d84388682e10caf96cc8e
mochibot <amusaur@gmail.com>
2019-08-23 16:16:05 -0700

    initial commit: set up basic file structure

---
+++ api/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json({ message: 'server is running go catch it' });
+})
+
+module.exports = server;
