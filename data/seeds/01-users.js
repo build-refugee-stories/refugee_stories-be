5020fe16f1eaf9a6e8079afb7af2d4e0453aaeca
mochibot <amusaur@gmail.com>
2019-08-24 17:58:42 -0700

    tweaked 01-users seed file

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,15 +1,10 @@
 const bcrypt = require('bcryptjs')
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('users').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('users').insert([
-        {
-          username: 'guest', 
-          password: bcrypt.hashSync('guest', 12)
-        }
-      ]);
-    });
+  return knex('users').insert([
+    {
+      username: 'guest', 
+      password: bcrypt.hashSync('guest', 12)
+    }
+  ]);
 };
